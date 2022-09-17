c708d81d89f194729b004597da5f65f38052bcfc
MaksimMischenko <t89969534097@gmail.com>
2022-09-17 14:19:26 +0400

    initialization_models

--- main/java/com/kata/cinema/base/models/entity/Movie.java
+++ main/java/com/kata/cinema/base/models/entity/Movie.java
@@ -40,6 +40,14 @@ public class Movie {
     @Column(name = "origin_name")  // оригинальное наименование фильма
     private String originName;
 
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private List<Content> contents;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private List<Score> scores;
+
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "collection_movie",
             joinColumns = @JoinColumn(name = "movie_id"))
@@ -50,33 +58,30 @@ public class Movie {
             joinColumns = @JoinColumn(name = "movie_id"))
     private List<Genre> movieId;
 
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinTable(name = "folder_movies_to_movie",
-//            joinColumns = @JoinColumn(name = "movies_id"))
-//    private List<FoldersMovie> foldersMovies;
-//
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinTable(name = "movie_person",
-//            joinColumns = @JoinColumn(name = "movie_id"))
-//    private List<Person> persons;
-//
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinTable(name = "awards_ceremony_result",
-//            joinColumns = @JoinColumn(name = "movie_id"))
-//    private List<AwardsCeremonyResult> awardsCeremonyResults;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "news_movie",
+            joinColumns = @JoinColumn(name = "movie_id"))
+    private List<Genre> news;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "movie_id")
-    private List<Content> contents;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "folder_movies_to_movie",
+            joinColumns = @JoinColumn(name = "movies_id"))
+    private List<FoldersMovie> foldersMovies;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "movie_id")
-    private List<Score> scores;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "movie_person",
+            joinColumns = @JoinColumn(name = "movie_id"))
+    private List<Person> persons;
 
-    public Movie(long id, String name, String countries, String dataRelease, int rars, double mpaa, String time,
-                 String description, String type, String originName, List<Collection> collections, List<Genre> movieId,
-                 // List<FoldersMovie> foldersMovies, List<Person> persons, List<AwardsCeremonyResult> awardsCeremonyResults,
-                 List<Content> contents, List<Score> scores) {
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "awards_ceremony_result",
+            joinColumns = @JoinColumn(name = "movie_id"))
+    private List<AwardsCeremonyResult> awardsCeremonyResults;
+
+    public Movie(long id, String name, String countries, String dataRelease, Integer rars, Double mpaa, String time,
+                 String description, String type, String originName, List<Content> contents, List<Score> scores,
+                 List<Collection> collections, List<Genre> movieId, List<Genre> news, List<FoldersMovie> foldersMovies,
+                 List<Person> persons, List<AwardsCeremonyResult> awardsCeremonyResults) {
         this.id = id;
         this.name = name;
         this.countries = countries;
@@ -87,13 +92,14 @@ public class Movie {
         this.description = description;
         this.type = type;
         this.originName = originName;
-        this.collections = collections;
-        this.movieId = movieId;
-//        this.foldersMovies = foldersMovies;
-//        this.persons = persons;
-//        this.awardsCeremonyResults = awardsCeremonyResults;
         this.contents = contents;
         this.scores = scores;
+        this.collections = collections;
+        this.movieId = movieId;
+        this.news = news;
+        this.foldersMovies = foldersMovies;
+        this.persons = persons;
+        this.awardsCeremonyResults = awardsCeremonyResults;
     }
 
     public long getId() {
@@ -128,19 +134,19 @@ public class Movie {
         this.dataRelease = dataRelease;
     }
 
-    public int getRars() {
+    public Integer getRars() {
         return rars;
     }
 
-    public void setRars(int rars) {
+    public void setRars(Integer rars) {
         this.rars = rars;
     }
 
-    public double getMpaa() {
+    public Double getMpaa() {
         return mpaa;
     }
 
-    public void setMpaa(double mpaa) {
+    public void setMpaa(Double mpaa) {
         this.mpaa = mpaa;
     }
 
@@ -176,46 +182,6 @@ public class Movie {
         this.originName = originName;
     }
 
-    public List<Collection> getCollections() {
-        return collections;
-    }
-
-    public void setCollections(List<Collection> collections) {
-        this.collections = collections;
-    }
-
-    public List<Genre> getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(List<Genre> movieId) {
-        this.movieId = movieId;
-    }
-
-//    public List<FoldersMovie> getFoldersMovies() {
-//        return foldersMovies;
-//    }
-//
-//    public void setFoldersMovies(List<FoldersMovie> foldersMovies) {
-//        this.foldersMovies = foldersMovies;
-//    }
-//
-//    public List<Person> getPersons() {
-//        return persons;
-//    }
-//
-//    public void setPersons(List<Person> persons) {
-//        this.persons = persons;
-//    }
-//
-//    public List<AwardsCeremonyResult> getAwardsCeremonyResults() {
-//        return awardsCeremonyResults;
-//    }
-//
-//    public void setAwardsCeremonyResults(List<AwardsCeremonyResult> awardsCeremonyResults) {
-//        this.awardsCeremonyResults = awardsCeremonyResults;
-//   }
-
     public List<Content> getContents() {
         return contents;
     }
@@ -232,6 +198,54 @@ public class Movie {
         this.scores = scores;
     }
 
+    public List<Collection> getCollections() {
+        return collections;
+    }
+
+    public void setCollections(List<Collection> collections) {
+        this.collections = collections;
+    }
+
+    public List<Genre> getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(List<Genre> movieId) {
+        this.movieId = movieId;
+    }
+
+    public List<Genre> getNews() {
+        return news;
+    }
+
+    public void setNews(List<Genre> news) {
+        this.news = news;
+    }
+
+    public List<FoldersMovie> getFoldersMovies() {
+        return foldersMovies;
+    }
+
+    public void setFoldersMovies(List<FoldersMovie> foldersMovies) {
+        this.foldersMovies = foldersMovies;
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    public List<AwardsCeremonyResult> getAwardsCeremonyResults() {
+        return awardsCeremonyResults;
+    }
+
+    public void setAwardsCeremonyResults(List<AwardsCeremonyResult> awardsCeremonyResults) {
+        this.awardsCeremonyResults = awardsCeremonyResults;
+    }
+
     public Movie() {
         super();
     }
@@ -246,21 +260,21 @@ public class Movie {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Movie movie = (Movie) o;
-        return id == movie.id && rars == movie.rars && Double.compare(movie.mpaa, mpaa) == 0
-                && Objects.equals(name, movie.name) && Objects.equals(countries, movie.countries)
-                && Objects.equals(dataRelease, movie.dataRelease) && Objects.equals(time, movie.time)
+        return id == movie.id && Objects.equals(name, movie.name) && Objects.equals(countries, movie.countries)
+                && Objects.equals(dataRelease, movie.dataRelease) && Objects.equals(rars, movie.rars)
+                && Objects.equals(mpaa, movie.mpaa) && Objects.equals(time, movie.time)
                 && Objects.equals(description, movie.description) && Objects.equals(type, movie.type)
-                && Objects.equals(originName, movie.originName) && Objects.equals(collections, movie.collections)
-                && Objects.equals(movieId, movie.movieId)
-                // && Objects.equals(foldersMovies, movie.foldersMovies) && Objects.equals(persons, movie.persons) && Objects.equals(awardsCeremonyResults, movie.awardsCeremonyResults)
-                && Objects.equals(contents, movie.contents) && Objects.equals(scores, movie.scores);
+                && Objects.equals(originName, movie.originName) && Objects.equals(contents, movie.contents)
+                && Objects.equals(scores, movie.scores) && Objects.equals(collections, movie.collections)
+                && Objects.equals(movieId, movie.movieId) && Objects.equals(news, movie.news)
+                && Objects.equals(foldersMovies, movie.foldersMovies) && Objects.equals(persons, movie.persons)
+                && Objects.equals(awardsCeremonyResults, movie.awardsCeremonyResults);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, countries, dataRelease, rars, mpaa, time, description, type, originName, collections, movieId,
-                // foldersMovies, persons, awardsCeremonyResults,
-                contents, scores);
+        return Objects.hash(id, name, countries, dataRelease, rars, mpaa, time, description, type, originName, contents,
+                scores, collections, movieId, news, foldersMovies, persons, awardsCeremonyResults);
     }
 
     @Override
@@ -276,13 +290,14 @@ public class Movie {
                 ", description='" + description + '\'' +
                 ", type='" + type + '\'' +
                 ", originName='" + originName + '\'' +
-                ", collections=" + collections +
-                ", movieId=" + movieId +
-//                ", foldersMovies=" + foldersMovies +
-//                ", persons=" + persons +
-//                ", awardsCeremonyResults=" + awardsCeremonyResults +
                 ", contents=" + contents +
                 ", scores=" + scores +
+                ", collections=" + collections +
+                ", movieId=" + movieId +
+                ", news=" + news +
+                ", foldersMovies=" + foldersMovies +
+                ", persons=" + persons +
+                ", awardsCeremonyResults=" + awardsCeremonyResults +
                 '}';
     }
 }
