0f27741d180b164e927f10ac537930fea488f876
salomonfx <okk.dir@gmail.com>
2022-09-23 22:24:44 +0300

    initialization_models_3

--- src/main/java/com/kata/cinema/base/models/entity/MoviePerson.java
+++ src/main/java/com/kata/cinema/base/models/entity/MoviePerson.java
@@ -5,6 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,7 +16,50 @@ import javax.persistence.*;
 @EqualsAndHashCode(of = {"id"})
 @Table(name = "movie_person")
 public class MoviePerson {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @Embeddable
+    @EqualsAndHashCode(of = {"personId", "movieId"})
+    public static class Id implements Serializable {
 
+        @Column(name = "person_id")
+        private Long personId;
 
+        @Column(name = "movie_id")
+        private Long movieId;
 
+        public Id(Long personId, Long movieId) {
+            this.personId = personId;
+            this.movieId = movieId;
+        }
+    }
+    @EmbeddedId
+    private Id id;
+
+    @ManyToOne(targetEntity = Person.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id", insertable = false, updatable = false)
+    private Person person;
+
+    @ManyToOne(targetEntity = Movie.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", insertable = false, updatable = false)
+    private Movie movie;
+
+    @ManyToMany(cascade = CascadeType.REFRESH)
+    @JoinTable(name = "movie_person_to_profession",
+            joinColumns = {@JoinColumn(name = "movie_id", insertable = false, updatable = false),
+                    @JoinColumn(name = "person_id", insertable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "profession_id")})
+    private Set<Profession> professions;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_character")
+    private TypeCharacter type;
+
+    @Column(name = "name_role")
+    private String nameRole;
 }
+
+
+
+
