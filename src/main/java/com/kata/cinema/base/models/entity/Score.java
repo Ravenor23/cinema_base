186f64024f1af3d8e6aacd5b37802e4993113590
zaborskiy <zaborskiy_ai@mail.ru>
2022-09-26 17:48:21 +0300

    init

--- src/main/java/com/kata/cinema/base/models/entity/Score.java
+++ src/main/java/com/kata/cinema/base/models/entity/Score.java
@@ -17,39 +17,15 @@ public class Score {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "movie_id")
-    private String movieId;
-
-    @Column(name = "user_id")
-    private String userId;
-
     @Column(name = "score")
-    private String score;
+    private Integer score;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
     private Movie movie;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Score score1 = (Score) o;
-        return id == score1.id && Objects.equals(movieId, score1.movieId) && Objects.equals(userId, score1.userId) && Objects.equals(score, score1.score) && Objects.equals(movie, score1.movie);
-    }
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, movieId, userId, score, movie);
-    }
-
-    @Override
-    public String toString() {
-        return "Score{" +
-                "id=" + id +
-                ", movieId='" + movieId + '\'' +
-                ", userId='" + userId + '\'' +
-                ", score='" + score + '\'' +
-                ", movie=" + movie +
-                '}';
-    }
 }
