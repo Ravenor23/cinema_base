4ca18a3909cce5d2cbefd0dea78f4d4318c8e842
MaksimMischenko <t89969534097@gmail.com>
2022-10-07 00:34:28 +0400

    grade_movies

--- main/java/com/kata/cinema/base/init/DataInit.java
+++ main/java/com/kata/cinema/base/init/DataInit.java
@@ -27,28 +27,37 @@ public class DataInit {
         this.collectionService = collectionService;
     }
 
-    @PostConstruct
-    public void createMovie() {
-        List<Genre> listGenre = new ArrayList<>();
-        List<Collection> listCollection = new ArrayList<>();
+    List<Genre> listGenre = new ArrayList<>();
+    List<Collection> listCollection = new ArrayList<>();
 
+    @PostConstruct
+    public void createGenre() {
+        Genre genre = new Genre();
         for (int i = 1; i < 11; i++) {
-            Genre genre = new Genre();
             genre.setName("Genre" + i);
             genreService.save(genre);
             listGenre.add(genre);
         }
+    }
+
+    @PostConstruct
+    public void createCollection() {
+        Collection collection = new Collection();
         for (int i = 1; i < 21; i++) {
-            Collection collection = new Collection();
             collection.setName("Collection" + i);
             if (i >= 15) {
                 collection.setEnable(false);
             } else {
                 collection.setEnable(true);
             }
+
             collectionService.save(collection);
             listCollection.add(collection);
         }
+    }
+
+    @PostConstruct
+    public void createMovie() {
         for (int i = 1; i < 101; i++) {
             Movie movie = new Movie();
             movie.setName("Movie" + i);
@@ -58,29 +67,22 @@ public class DataInit {
                     "описание описание описание описание");
             movie.setMpaa(MPAA.generateRandomMPAA());
             movie.setRars(RARS.generateRandomRARS());
+
             Collections.shuffle(listGenre);
             if (i % 2 == 0) {
                 movie.setGenres(listGenre.subList(0, 3));
             } else {
                 movie.setGenres(listGenre.subList(0, 1));
             }
+
             Collections.shuffle(listCollection);
             if (i % 2 == 0) {
                 movie.setCollections(listCollection.subList(0, 15));
             } else {
                 movie.setCollections(listCollection.subList(0, 5));
             }
+
             movieService.save(movie);
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
