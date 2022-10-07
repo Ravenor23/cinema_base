31f177dd5fc899a803f9a2d0127aad6e73ea5c71
Alexay <sapytn@gmail.com>
2022-10-07 17:30:55 +0300

    intermediate push

---
+++ src/main/java/com/kata/cinema/base/webapp/controllers/unauthorized/MovieRestController.java
@@ -0,0 +1,14 @@
+package com.kata.cinema.base.webapp.controllers.unauthorized;
+
+import com.kata.cinema.base.models.dto.response.ReviewResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieRestController {
+
+//  @GetMapping("/api/movies/{id}/reviews/page/{pageNumber}?itemsOnPage={itemsOnPage}&typeReview={typeReview}&reviewSortType={reviewSortType}")
+
+
+}
