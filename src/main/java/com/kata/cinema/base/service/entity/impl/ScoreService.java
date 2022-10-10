e2b2f870eac015b99c6bd278a50ffd684c53ca40
MaksimMischenko <t89969534097@gmail.com>
2022-10-11 00:15:53 +0400

    11_2_grade_movies

--- src/main/java/com/kata/cinema/base/service/entity/impl/ScoreService.java
+++ src/main/java/com/kata/cinema/base/service/entity/impl/ScoreService.java
@@ -1,4 +1,4 @@
-package src.src.main.java.com.kata.cinema.base.service.entity.impl;
+package com.kata.cinema.base.service.entity.impl;
 
 import com.kata.cinema.base.models.dto.response.ScoreResponseDto;
 
--- src/main/java/com/kata/cinema/base/service/entity/impl/ScoreServiceImp.java
+++ src/main/java/com/kata/cinema/base/service/entity/impl/ScoreServiceImp.java
@@ -1,25 +1,30 @@
-package src.src.main.java.com.kata.cinema.base.service.entity.impl;
+package com.kata.cinema.base.service.entity.impl;
 
 import com.kata.cinema.base.models.dto.response.ScoreResponseDto;
 import com.kata.cinema.base.models.entity.Score;
+import com.kata.cinema.base.repositories.MovieRepository;
 import com.kata.cinema.base.repositories.ScoreRepository;
+import com.kata.cinema.base.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ScoreServiceImp implements ScoreService{
+public class ScoreServiceImp implements ScoreService {
     private final ScoreRepository scoreRepository;
     private final MovieRepository movieRepository;
 
+    private final UserRepository userRepository;
 
-    public ScoreServiceImp(ScoreRepository scoreRepository, MovieRepository movieRepository) {
+    public ScoreServiceImp(ScoreRepository scoreRepository, MovieRepository movieRepository, UserRepository userRepository) {
         this.scoreRepository = scoreRepository;
         this.movieRepository = movieRepository;
+        this.userRepository = userRepository;
     }
+
     @Override
     public void save(ScoreResponseDto scoreResponseDto) {
         Score score = new Score();
         score.setMovie(movieRepository.findById(scoreResponseDto.getMovieId()).orElseThrow());
-        score.setUserId(scoreResponseDto.getUserId().toString());
+        score.setUser(userRepository.getById(scoreResponseDto.getUserId()));
         score.setScore(scoreResponseDto.getScore());
         scoreRepository.save(score);
     }
