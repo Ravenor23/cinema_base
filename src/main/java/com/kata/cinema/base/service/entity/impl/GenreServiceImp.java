93e925dae697b52707617cf1c208d13551724067
Alexay <sapytn@gmail.com>
2022-10-10 22:49:56 +0300

    reviews

--- src/main/java/com/kata/cinema/base/service/entity/impl/GenreServiceImp.java
+++ src/main/java/com/kata/cinema/base/service/entity/impl/GenreServiceImp.java
@@ -1,6 +1,8 @@
 package src.src.main.java.com.kata.cinema.base.service.entity.impl;
 
 import com.kata.cinema.base.models.entity.Genre;
+import com.kata.cinema.base.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import src.src.main.java.com.kata.cinema.base.service.entity.impl.GenreService;
 
@@ -8,6 +10,7 @@ import src.src.main.java.com.kata.cinema.base.service.entity.impl.GenreService;
 public class GenreServiceImp implements GenreService {
     private final GenreRepository genreRepository;
 
+    @Autowired
     public GenreServiceImp(GenreRepository genreRepository) {
         this.genreRepository = genreRepository;
     }
--- src/main/java/com/kata/cinema/base/webapp/controllers/unauthorized/MovieRestController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/unauthorized/MovieRestController.java
@@ -1,14 +1,40 @@
 package com.kata.cinema.base.webapp.controllers.unauthorized;
 
+import com.kata.cinema.base.models.dto.response.PageDto;
 import com.kata.cinema.base.models.dto.response.ReviewResponseDto;
+import com.kata.cinema.base.models.enums.ReviewSortType;
+import com.kata.cinema.base.models.enums.TypeReview;
+import com.kata.cinema.base.service.PaginationDtoService;
+import java.util.HashMap;
+import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class MovieRestController {
 
-//  @GetMapping("/api/movies/{id}/reviews/page/{pageNumber}?itemsOnPage={itemsOnPage}&typeReview={typeReview}&reviewSortType={reviewSortType}")
+  private final PaginationDtoService<ReviewResponseDto> paginationDtoService;
 
+  @Autowired
+  public MovieRestController(PaginationDtoService<ReviewResponseDto> paginationDtoService) {
+    this.paginationDtoService = paginationDtoService;
+  }
+
+  @GetMapping("/api/movies/{id}/reviews/page/{pageNumber}?itemsOnPage={itemsOnPage}&typeReview={typeReview}&reviewSortType={reviewSortType}")
+  public PageDto<ReviewResponseDto> getPageReview(@PathVariable("id") Long id, @PathVariable("pageNumber") Integer pageNumber,
+      @RequestParam("itemsOnPage") Integer itemsOnPage,
+      @RequestParam(value = "typeReview", required = false) TypeReview typeReview,
+      @RequestParam(value = "reviewSortType", defaultValue = "DATE_ASC") ReviewSortType reviewSortType){
+    HashMap<String, Object> parameters = new HashMap<>();
+    parameters.put("id", id);
+    parameters.put("typeReview", typeReview);
+    parameters.put("reviewSortType", reviewSortType);
+    return paginationDtoService.getPageDtoWithParameters(pageNumber, itemsOnPage, parameters);
+  }
 
 }
