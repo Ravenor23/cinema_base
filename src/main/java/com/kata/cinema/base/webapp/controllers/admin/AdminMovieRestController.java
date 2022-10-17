e8b9e849ef5a0fcad30bd6273dd2c2db42da1f9b
MaksimMischenko <t89969534097@gmail.com>
2022-10-17 22:06:33 +0400

    12

--- src/main/java/com/kata/cinema/base/webapp/controllers/admin/AdminMovieRestController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/admin/AdminMovieRestController.java
@@ -1,5 +1,6 @@
 package com.kata.cinema.base.webapp.controllers.admin;
 
+import com.kata.cinema.base.service.entity.impl.ContentServiceImp;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,12 +11,19 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api/moderator/movie")
 public class AdminMovieRestController {
+    private final ContentServiceImp contentService;
+
+    public AdminMovieRestController(ContentServiceImp contentService) {
+        this.contentService = contentService;
+    }
 
     @PostMapping("/{id}/uploadPreview")
-    public ResponseEntity<String> uploadPreview(@PathVariable("id") String movieId,
+    public ResponseEntity<?> uploadPreview(@PathVariable("id") Long movieId,
                                                 @RequestParam("image") MultipartFile file) throws IOException {
-        File convFile = new File(System.getProperty("/uploads/movies/preview") + "/" + movieId);
+
+        File convFile = new File("/uploads/movies/preview");
         file.transferTo(convFile);
+        contentService.saveFile(movieId, convFile);
         return ResponseEntity.ok("upload");
     }
 }
