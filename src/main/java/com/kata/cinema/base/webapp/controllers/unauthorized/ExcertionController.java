9a61faeff53e75ac9bf8a1cd29bccb174e1320b1
Murzin34 <murzinvolg@yandex.ru>
2022-10-20 16:47:13 +0300

    31-excertion-ver2

--- src/main/java/com/kata/cinema/base/webapp/controllers/unauthorized/ExcertionController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/unauthorized/ExcertionController.java
@@ -3,6 +3,7 @@ package com.kata.cinema.base.webapp.controllers.unauthorized;
 import com.kata.cinema.base.models.dto.request.ExcertionRequestDto;
 import com.kata.cinema.base.models.dto.response.ExcertionResponseDto;
 import com.kata.cinema.base.models.dto.response.PageDto;
+import com.kata.cinema.base.service.PaginationDtoService;
 import com.kata.cinema.base.service.dto.impl.ExcertionService;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,20 +13,26 @@ import org.springframework.web.bind.annotation.*;
 public class ExcertionController {
 
     private final ExcertionService excertionService;
+    private final PaginationDtoService paginationDtoService;
 
-    public ExcertionController(ExcertionService excertionService) {
+    public ExcertionController(ExcertionService excertionService, PaginationDtoService paginationDtoService) {
         this.excertionService = excertionService;
+        this.paginationDtoService = paginationDtoService;
     }
 
     @GetMapping("/api/movies/{id}/excertions/page/{pageNumber}?itemsOnPage=")
     public PageDto<ExcertionResponseDto> findExcertionPageDtoByIdFromMovies(@PathVariable("id") Long id,
-                                                                            @PathVariable PageDto pageNumber) {
+                                                                            @PathVariable("pageNumber") Integer currentPage,
+                                                                            @RequestParam("itemsOnPage") Integer itemsOnPage) {
+        paginationDtoService.getPageDto(currentPage, itemsOnPage);
         return excertionService.findExcertionPageDtoById(id);
     }
 
-    @GetMapping("/api/persons/{id}/excertions/page/{pageNumber}?itemsOnPage=")
+    @GetMapping("/api/person/{id}/excertions/page/{pageNumber}?itemsOnPage=")
     public PageDto<ExcertionResponseDto> findExcertionPageDtoByIdFromPerson(@PathVariable("id") Long id,
-                                                                            @PathVariable PageDto pageNumber) {
+                                                                            @PathVariable("pageNumber") Integer currentPage,
+                                                                            @RequestParam("itemsOnPage") Integer itemsOnPage) {
+        paginationDtoService.getPageDto(currentPage, itemsOnPage);
         return excertionService.findExcertionPageDtoById(id);
     }
 
