b74a64105403cfead179c88c5292a0ccb35f51b5
Konstantin Sekretarev <gettbox@mail.ru>
2022-10-25 20:20:29 +0300

    task#34 correction 3

--- src/main/java/com/kata/cinema/base/dao/impl/TopMovieDaoImpl.java
+++ src/main/java/com/kata/cinema/base/dao/impl/TopMovieDaoImpl.java
@@ -1,35 +1,29 @@
 package com.kata.cinema.base.dao.impl;
 
 import com.kata.cinema.base.dao.TopMovieDao;
+import com.kata.cinema.base.repositories.TopMovieRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.CallableStatement;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Objects;
 
 @Repository
 public class TopMovieDaoImpl implements TopMovieDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final TopMovieRepository topMovieRepository;
 
-    public TopMovieDaoImpl(JdbcTemplate jdbcTemplate) {
+    public TopMovieDaoImpl(JdbcTemplate jdbcTemplate, TopMovieRepository topMovieRepository) {
         this.jdbcTemplate = jdbcTemplate;
+        this.topMovieRepository = topMovieRepository;
     }
 
     public void set(int scoreAmount, int limit) throws SQLException, IOException {
 
         jdbcTemplate.execute(Files.readString(Paths.get("src/main/resources/static/data/top_movies_set.sql")));
-
-        Connection connection = Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection();
-
-        CallableStatement callableStatement = connection.prepareCall("call top_movie_set(?,?)");
-        callableStatement.setInt(1, scoreAmount);
-        callableStatement.setInt(2, limit);
-        callableStatement.execute();
+        topMovieRepository.set(scoreAmount, limit);
     }
 }
