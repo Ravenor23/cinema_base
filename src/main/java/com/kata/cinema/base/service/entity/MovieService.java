ea5642351f538f4d8bcacbbf1dbbc1b666f7983a
Alexay <sapytn@gmail.com>
2022-11-04 19:34:22 +0300

    adding_movie

--- src/main/java/com/kata/cinema/base/service/entity/MovieService.java
+++ src/main/java/com/kata/cinema/base/service/entity/MovieService.java
@@ -1,11 +1,13 @@
 package com.kata.cinema.base.service.entity;
 
+import com.kata.cinema.base.models.dto.request.MovieRequestDto;
 import com.kata.cinema.base.models.entity.Movie;
-
 import java.util.List;
 
 public interface MovieService {
     void save(Movie movie);
+    void save(MovieRequestDto movieRequestDto);
     List<Movie> getAll();
     Movie findById(Long id);
+    void updateMovie(Long id, MovieRequestDto movieRequestDto);
 }
